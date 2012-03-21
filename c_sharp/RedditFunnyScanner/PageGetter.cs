c7409c7310276303aa063149552dc418bc80a7ec
Alexey Kryuchkov <alexey.kruchkov@gmail.com>
2012-03-21 20:43:05 +0400

    first commit

---
+++ c_sharp/RedditFunnyScanner/PageGetter.cs
@@ -0,0 +1,50 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Alexey
+ * Date: 15.06.2010
+ * Time: 21:14
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Net;
+using System.IO;
+
+namespace RedditFunnyScanner
+{
+	/// <summary>
+	/// Description of PageGetter.
+	/// </summary>
+	public class PageGetter
+	{
+		public static string GetPage(string Url)
+		{
+					// Open a connection
+		    HttpWebRequest WebRequestObject = (HttpWebRequest)HttpWebRequest.Create(Url);
+		 
+		    // You can also specify additional header values like 
+		    // the user agent or the referer:
+		    WebRequestObject.UserAgent	= ".NET Framework/2.0";
+		    WebRequestObject.Referer	= "http://www.example.com/";
+		 
+		    // Request response:
+		    WebResponse Response = WebRequestObject.GetResponse();
+		 
+		    // Open data stream:
+		    Stream WebStream = Response.GetResponseStream();
+		 
+		    // Create reader object:
+		    StreamReader Reader = new StreamReader(WebStream);
+		 
+		    // Read the entire stream content:
+		    string PageContent = Reader.ReadToEnd();
+		 
+		    // Cleanup
+		    Reader.Close();
+		    WebStream.Close();
+		    Response.Close();
+		 
+		    return PageContent;
+		}
+	}
+}
---
+++ c_sharp/RedditFunnyScanner/Program.cs
@@ -0,0 +1,82 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Alexey
+ * Date: 15.06.2010
+ * Time: 21:03
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using System.Linq;
+
+namespace RedditFunnyScanner
+{
+	class Program
+	{
+		const string RedditUrl = @"http://www.reddit.com/r/funny/";
+		static Regex rxLink = new Regex("<a class=\"title \" href=\"(.*?)\" >(.*?)</a>", RegexOptions.Compiled);
+		static Regex rxNextPage = new Regex("<a href=\"(http://www.reddit.com/r/funny/.count=[0-9]+&amp;after=.*?)\"", RegexOptions.Compiled);
+		
+		public static void Main(string[] args)
+		{
+			new Program().Run();
+		}
+		
+		void Run()
+		{
+			string url = RedditUrl;
+			
+			List<RedditLink> alllinks = new List<RedditLink>();
+			
+			while (true)
+			{
+				string page = PageGetter.GetPage(url);
+				List<RedditLink> links = ProcessPage(page);
+				alllinks.AddRange(links);
+				string nextUrl = rxNextPage.Match(page).Groups[1].Value;
+					
+				if (nextUrl != "")
+					url = nextUrl;
+				else
+					break;
+			}
+			
+			string[] lines = (
+				from link in alllinks 
+				where link.Url.IndexOf(".jpg") > 0
+				||  link.Url.IndexOf(".gif") > 0
+				|| link.Url.IndexOf(".png") > 0
+				|| link.Url.IndexOf("imgur.com") > 0
+				select  string.Format("<p><a href=\"{0}\">{1}</a></p>", link.Url, link.Title)
+			).ToArray();
+			
+			string filename = Path.GetTempFileName() + ".htm";
+			File.WriteAllLines(filename, lines);
+			
+			System.Diagnostics.Process.Start(filename);
+				
+		}
+		
+		List<RedditLink> ProcessPage(string page)
+		{
+			var matches = rxLink.Matches(page);
+			var list = new List<RedditLink>();
+			
+			foreach(Match match in matches)
+			{
+				list.Add(new RedditLink() { Url = match.Groups[1].Value, Title = match.Groups[2].Value });
+			}
+			
+			return list;
+		}
+	}
+	
+	class RedditLink
+	{
+		public string Title { get; set; }
+		public string Url { get; set; }
+	}
+}
